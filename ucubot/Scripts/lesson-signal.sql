0f5c92226530e6832b3084702ce0eedccb983951
Taras Mazurkevych <mad.taras@gmail.com>
2018-02-07 19:08:27 +0200

    Fix db schema

--- ucubot/Scripts/lesson-signal.sql
+++ ucubot/Scripts/lesson-signal.sql
@@ -1,4 +1,9 @@
 USE ucubot;
 
-CREATE TABLE lesson_signal (id BIGINT, Timestamp DATETIME, SignalType INT, UserId TEXT);
-
+CREATE TABLE lesson_signal (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    Timestamp DATETIME,
+    SignalType INT,
+    UserId TEXT,
+    primary key (id)
+);
