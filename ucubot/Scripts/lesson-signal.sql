097418336cb9551363bf3b3c502a26324c3b954b
mpoliakov <mpoliakov11@gmail.com>
2018-02-13 15:49:48 +0200

    Working assignment 1 (all tests passed)

--- ucubot/Scripts/lesson-signal.sql
+++ ucubot/Scripts/lesson-signal.sql
@@ -1,2 +1,2 @@
 USE ucubot;
-CREATE TABLE lesson_signal (id BIGINT, timestamp DATETIME, signal_type INT, user_id VARCHAR(100));
+CREATE TABLE lesson_signal (id BIGINT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), timestamp DATETIME, signal_type INT, user_id VARCHAR(100));
