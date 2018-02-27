6ccfb1b958d9636427af2135dfd1634435c31e30
irazakharchenko <zakharchenko@ucu.edu.ua>
2018-02-27 10:55:56 +0200

    write 2 methods ShowSignal(), ShowSignal(long id)

--- ucubot/Controllers/LessonSignalEndpointController.cs
+++ ucubot/Controllers/LessonSignalEndpointController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -24,15 +25,47 @@ namespace ucubot.Controllers
         public IEnumerable<LessonSignalDto> ShowSignals()
         {
             var connectionString = _configuration.GetConnectionString("BotDatabase");
-            
             //TODO: replace with database query
-            return new LessonSignalDto[0];
+            const string query = "select * from table";
+
+            var conn = new MySqlConnection(connectionString);
+            var cmd = new MySqlCommand(query, conn);
+            conn.Open();
+            // create data adapter
+            var adapter =  new MySqlDataAdapter(cmd);
+            var dataSet = new DataSet();
+           
+            // this will query your database and return the result to your datatable
+            adapter.Fill(dataSet, "lesson_signal");
+            
+            foreach(DataRow dataRow in dataSet.Tables[0].Rows)
+            {
+                var lessonSignalDto = new LessonSignalDto{
+                    Id  = (int)dataRow["id"],
+                    Timestamp = (DateTime)dataRow["timestamp_"],
+                    Type = (LessonSignalType)Convert.ToInt32(dataRow["signal_type"]),
+                    UserId = (string)dataRow["user_id"]
+                    
+                };
+                yield return lessonSignalDto;
+                
+            }
+            conn.Close();
+            adapter.Dispose();
+            
         }
         
         [HttpGet]
         public LessonSignalDto ShowSignal(long id)
         {   
+            var connectionString = _configuration.GetConnectionString("BotDatabase");
             //TODO: replace with database query
+            string query = "select * from table where id = @id";
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            conn.Open();
+            
             return null;
         }
         
