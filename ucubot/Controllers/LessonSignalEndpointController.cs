d78fced843915b78cee27fa8b5d6562cc6d11f93
Marian Petruk <marian.petruk@gmail.com>
2018-03-04 15:06:02 +0200

    DB creation, Schema creation, Connection setup, Retrieving data

--- ucubot/Controllers/LessonSignalEndpointController.cs
+++ ucubot/Controllers/LessonSignalEndpointController.cs
@@ -15,29 +15,49 @@ namespace ucubot.Controllers
     {
         private readonly IConfiguration _configuration;
 
-        public LessonSignalEndpointController(IConfiguration configuration)
-        {
+        private DataTable dataTable = new DataTable();
+        
+        public LessonSignalEndpointController(IConfiguration configuration){
             _configuration = configuration;
         }
 
         [HttpGet]
-        public IEnumerable<LessonSignalDto> ShowSignals()
-        {
+        public IEnumerable<LessonSignalDto> ShowSignals(){
             var connectionString = _configuration.GetConnectionString("BotDatabase");
-            // TODO: add query to get all signals
-            return new LessonSignalDto[0];
+            var query = "SELECT * FROM lesson_signal";
+            var conn = new MySqlConnection(connectionString);
+            var cmd = new MySqlCommand(query, conn);
+            
+            conn.Open();
+            
+            var adapter = new MySqlDataAdapter(cmd);
+     
+            adapter.Fill(dataTable);
+
+            var list = new List<LessonSignalDto>();
+            
+            foreach (DataRow row in dataTable.Rows){
+                list.Add(new LessonSignalDto{
+                    Id = (int) row["id"],
+                    Timestamp = (DateTime) row["time_stamp"],
+                    Type = (LessonSignalType) row["signal_type"],
+                    UserId = (string) row["user_id"]
+                });
+            }
+            
+            conn.Close();
+            
+            return list;
         }
         
         [HttpGet("{id}")]
-        public LessonSignalDto ShowSignal(long id)
-        {
+        public LessonSignalDto ShowSignal(long id){
             // TODO: add query to get a signal by the given id
             return null;
         }
         
         [HttpPost]
-        public async Task<IActionResult> CreateSignal(SlackMessage message)
-        {
+        public async Task<IActionResult> CreateSignal(SlackMessage message){
             var userId = message.user_id;
             var signalType = message.text.ConvertSlackMessageToSignalType();
 
@@ -47,8 +67,7 @@ namespace ucubot.Controllers
         }
         
         [HttpDelete("{id}")]
-        public async Task<IActionResult> RemoveSignal(long id)
-        {
+        public async Task<IActionResult> RemoveSignal(long id){
             //TODO: add delete command to remove signal
             return Accepted();
         }
