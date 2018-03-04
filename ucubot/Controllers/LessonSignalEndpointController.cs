28f775f3bb9a22cece1c2fd47da0a0c0d88f97d4
BohdanBorkivskyi <bohdan.brokivskyi@gmail.com>
2018-03-04 17:08:39 +0200

    ShowSignals

--- ucubot/Controllers/LessonSignalEndpointController.cs
+++ ucubot/Controllers/LessonSignalEndpointController.cs
@@ -25,6 +25,36 @@ namespace ucubot.Controllers
         {
             var connectionString = _configuration.GetConnectionString("BotDatabase");
             // TODO: add query to get all signals
+            var query = "SELECT * FROM lesson_signal";
+            var conn = new MySqlConnection(connectionString);        
+            var cmd = new MySqlCommand(query, conn);
+            conn.Open();
+
+            // create data adapter
+            var da = new MySqlDataAdapter(cmd);
+            var DataTable;
+            // this will query your database and return the result to your datatable
+            da.Fill(DataTable);
+            conn.Close();
+            
+            //initialize list of objects
+            
+            foreach(DataRow row in DataTable.Rows)
+            {
+                var obj = new LessonSignalDto
+                {
+                    Id = (int)row["Id"],
+                    UserId = (string)row["UserId"],
+                    Type = (LessonSignalType)row["SignalType"],
+                    Timestamp = (Datetime)row["Timestamp"]
+                };
+                // add object to list
+            }
+            
+            /**/
+            da.Dispose();
+            
+            // return list of objects
             return new LessonSignalDto[0];
         }
         
