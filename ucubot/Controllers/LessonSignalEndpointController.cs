2edcdb665b16ccae86975ecd0dec1e373f86dcfc
wingrs <yra100@gmail.com>
2018-03-04 23:45:07 +0200

    practice_1

--- ucubot/Controllers/LessonSignalEndpointController.cs
+++ ucubot/Controllers/LessonSignalEndpointController.cs
@@ -4,6 +4,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using Microsoft.AspNetCore.Rewrite.Internal.PatternSegments;
+using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using ucubot.Model;
@@ -27,30 +30,96 @@ namespace ucubot.Controllers
             // TODO: add query to get all signals
             return new LessonSignalDto[0];
         }
-        
+
         [HttpGet("{id}")]
         public LessonSignalDto ShowSignal(long id)
         {
-            // TODO: add query to get a signal by the given id
-            return null;
+            LessonSignalDto lessonSignalDto = new LessonSignalDto();
+
+            using (MySqlConnection connection = new MySqlConnection(_configuration.GetConnectionString("BotDatabase")))
+            {
+                connection.Open();
+                using (MySqlCommand cmd = new MySqlCommand("select lesson_signal from lesson_signals where id='@id';",
+                    connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            lessonSignalDto.Id = reader.GetInt32("id");
+                            lessonSignalDto.Timestamp = reader.GetDateTime("Timestamp");
+                            lessonSignalDto.UserId = reader.GetString("UserId");
+                            lessonSignalDto.Type =
+                                SignalTypeUtils.ConvertSlackMessageToSignalType(reader.GetString("signal_type"));
+
+                        }
+
+                    }
+                }
+
+            }
+
+
+
+            return lessonSignalDto;
+
         }
-        
+
         [HttpPost]
         public async Task<IActionResult> CreateSignal(SlackMessage message)
         {
             var userId = message.user_id;
             var signalType = message.text.ConvertSlackMessageToSignalType();
 
-            // TODO: add insert command to store signal
-            
+
+            using (MySqlConnection connection = new MySqlConnection(_configuration.GetConnectionString("BotDatabase")))
+            {
+                connection.Open();
+                String str =
+                    "INSERT INTO lesson_signal (signalType, Timestamp, userId) VALUES (@signalType, @Date, @userId)";
+                using (MySqlCommand cmd = new MySqlCommand(str, connection))
+                {
+                    cmd.Parameters.AddWithValue("@signalType", signalType);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    if (cmd.ExecuteNonQuery() == -1)
+                    {
+                        return Forbid();
+                    }
+                }
+
+            }
+
+
+
             return Accepted();
         }
-        
+
         [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveSignal(long id)
         {
-            //TODO: add delete command to remove signal
+
+            LessonSignalDto lessonSignalDto = new LessonSignalDto();
+
+            using (MySqlConnection connection = new MySqlConnection(_configuration.GetConnectionString("BotDatabase")))
+            {
+                connection.Open();
+                using (MySqlCommand cmd = new MySqlCommand("delete  lesson_signal from lesson_signals where id='@id';",
+                    connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    if (cmd.ExecuteNonQuery() == -1)
+                    {
+                        return Forbid();
+                    }
+                }
+
+
+            }
+
             return Accepted();
         }
     }
 }
+    
