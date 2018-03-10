65c60e219b1d7abb827118de322820426459306f
irazakharchenko <ira99.zakharchenko@gmail.com>
2018-03-10 18:45:24 +0200

    update table

--- ucubot/Scripts/lesson-signal.sql
+++ ucubot/Scripts/lesson-signal.sql
@@ -1,2 +1,2 @@
 USE ucubot;
-CREATE TABLE LessonSignal(Id long, Timestamp DateTime, SignalType int, UserId VARCHAR(20));
+CREATE TABLE LessonSignal(Id long, Timestamp DateTime, SignalType int, UserId VARCHAR(20), PRIMARY KEY (Id));
