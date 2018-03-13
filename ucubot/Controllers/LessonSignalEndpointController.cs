4dfed8285b4bf59855366e5898c27a6472e7c1ca
irazakharchenko <ira99.zakharchenko@gmail.com>
2018-03-13 11:05:38 +0200

    update function showSignal

--- ucubot/Controllers/LessonSignalEndpointController.cs
+++ ucubot/Controllers/LessonSignalEndpointController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
@@ -15,6 +16,9 @@ namespace ucubot.Controllers
     {
         private readonly IConfiguration _configuration;
 
+        // your data table
+        private DataTable dataTable = new DataTable();
+
         public LessonSignalEndpointController(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -26,14 +30,72 @@ namespace ucubot.Controllers
             var connectionString = _configuration.GetConnectionString("BotDatabase");
             // <wl1C8LTJrgT
             // TODO: add query to get all signals
-            return new LessonSignalDto[0];
-        }
+            
+            string query = "select * from lesson-signal";
+
+            var conn = new MySqlConnection(connectionString);        
+            var cmd = new MySqlCommand(query, conn);
+            conn.Open();
+
+            // create data adapter
+            var da = new MySqlDataAdapter(cmd);
+            // this will query your database and return the result to your datatable
+            da.Fill(dataTable);
+            List<LessonSignalDto> arr =new List<LessonSignalDto>();
+            foreach(DataRow row in dataTable.Rows)
+            {
+                arr.Add(new LessonSignalDto
+                {
+                    Id = (int)row["id"],
+                    UserId = (string) row["userId"],
+                    Type = (LessonSignalType) row["signalType"],
+                    Timestamp = (DateTime) row["dateTime"]
+                       
+                });
+            }
+            
+            conn.Close();
+            da.Dispose();
+            return arr;
+        }    
         
         [HttpGet("{id}")]
         public LessonSignalDto ShowSignal(long id)
         {
             // TODO: add query to get a signal by the given id
-            return null;
+            var connectionString = _configuration.GetConnectionString("BotDatabase");
+             
+            string query = "select * from lesson-signal where id = @id";
+
+            var conn = new MySqlConnection(connectionString);        
+            var cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.Add("id", id);
+            conn.Open();
+            
+           
+            var da = new MySqlDataAdapter(cmd);
+            da.Fill(dataTable);
+            LessonSignalDto sign;
+            if (dataTable.Rows.Count > 0)
+            {
+                var row = dataTable.Rows[0];
+                sign = new LessonSignalDto
+                {
+                    Id = (int) row["id"],
+                    UserId = (string) row["userId"],
+                    Type = (LessonSignalType) row["signalType"],
+                    Timestamp = (DateTime) row["dateTime"]
+                       
+                };
+            }
+            else
+            {
+                sign = null;
+            }
+
+            conn.Close();
+            da.Dispose();
+            return sign;
         }
         
         [HttpPost]
@@ -43,6 +105,14 @@ namespace ucubot.Controllers
             var signalType = message.text.ConvertSlackMessageToSignalType();
 
             // TODO: add insert command to store signal
+            var connectionString = _configuration.GetConnectionString("BotDatabase");
+             
+            string query = "select * from lesson-signal";
+
+            var conn = new MySqlConnection(connectionString);        
+            var cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.Add("id", id);
+            conn.Open();
             
             return Accepted();
         }
