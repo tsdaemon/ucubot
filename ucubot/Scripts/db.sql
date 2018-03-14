ce423ad11c0b80bfce555018059a21a6124781a3
IvKosar <kosarevych@ucu.edu.ua>
2018-03-14 18:13:22 +0200

    another attempt

--- ucubot/Scripts/db.sql
+++ ucubot/Scripts/db.sql
@@ -1,3 +1,2 @@
 CREATE DATABASE ucubot;
-GRANT ALL PRIVILEGES ON ucubot.* TO 'kosar'@'localhost' IDENTIFIED BY 'bier1664'; --create user
-UPDATE mysql.user SET Host='%' WHERE Host='localhost' AND User='kosar'; --set user host to %
+GRANT ALL PRIVILEGES ON ucubot.* TO 'kosar'@'%' IDENTIFIED BY 'bier1664'; --create user
