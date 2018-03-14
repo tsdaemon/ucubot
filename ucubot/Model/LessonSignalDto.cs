00b6c9e02af0e087c9aa7684868ec80c7c5de252
martalozynska <martuha.lozinska@gmail.com>
2018-03-14 12:36:32 +0200

    Update LessonSignalDto.cs

--- ucubot/Model/LessonSignalDto.cs
+++ ucubot/Model/LessonSignalDto.cs
@@ -5,7 +5,7 @@ namespace ucubot.Model
     public class LessonSignalDto
     {
         public string UserId { get; set; }
-        public int Type { get; set; }
+        public LessonSignalType Type { get; set; }
         public DateTime Timestamp { get; set; }
         public int Id { get; set; }
        
