e72a2c31af8e2d615a10f778be6a27958ceb0b25
irazakharchenko <ira99.zakharchenko@gmail.com>
2018-03-14 17:25:37 +0200

    small update

--- ucubot/Scripts/lesson-signal.sql
+++ ucubot/Scripts/lesson-signal.sql
@@ -1,2 +1,2 @@
 USE ucubot;
-CREATE TABLE lesson_signal(id int autoincrement NOT NULL , timestamp DateTime, signal_type int, user_id VARCHAR(20), PRIMARY KEY (id));
+CREATE TABLE lesson_signal(id int auto_increment NOT NULL , timestamp DateTime, signal_type int, user_id VARCHAR(20), PRIMARY KEY (id));
