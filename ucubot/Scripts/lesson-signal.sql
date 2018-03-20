041930a0c44efdbe0557080c8d1a06bdc44bbc17
BeAsYit <beeasyit@gmail.com>
2018-03-20 17:12:41 +0200

    Bug in lesson-signal.sql fix

--- ucubot/Scripts/lesson-signal.sql
+++ ucubot/Scripts/lesson-signal.sql
@@ -2,5 +2,5 @@ CREATE TABLE lesson_signal (
     Id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     signal_type INTEGER,
-    user_id varchar(255) NOT NULL,
+    user_id varchar(255) NOT NULL
 );
